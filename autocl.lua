947e7e15fb0e99e41b20fe2332319b5f3d17c757
Snowyno <124916660+Snowyno@users.noreply.github.com>
2024-04-30 14:23:35 -0300

    Update autocl.lua

--- autocl.lua
+++ autocl.lua
@@ -11,59 +11,37 @@ local function hop()
     local _place = game.PlaceId
     local _servers = "https://games.roblox.com/v1/games/".._place.."/servers/Public?sortOrder=Asc&limit=100"
     function ListServers(cursor)
-    local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-    return game:GetService("HttpService"):JSONDecode(Raw)
+        local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+        return game:GetService("HttpService"):JSONDecode(Raw)
     end
 
-    local Server, Next; repeat
-    local Servers = ListServers(Next)
-    Server = Servers.data[1]
-    Next = Servers.nextPageCursor
+    local Server, Next
+    repeat
+        local Servers = ListServers(Next)
+        Server = Servers.data[1]
+        Next = Servers.nextPageCursor
     until Server
 
-    game:GetService("TeleportService"):TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+    game:GetService("TeleportService"):TeleportToPlaceInstance(_place, Server.id, game.Players.LocalPlayer)
 end
 
-repeat 
-    wait(5)
-    firetouchtransmitter(workspace.__THINGS.Instances.ClawMachine:WaitForChild('Teleports').Enter, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
-until workspace.__THINGS.__INSTANCE_CONTAINER.Active:FindFirstChild('ClawMachine')
-
 while true do
-    for i,v in workspace.__THINGS.__INSTANCE_CONTAINER.Active.ClawMachine.Items:GetChildren() do
-        for j,k in v:GetChildren() do
+    valids = {} -- Limpa a tabela de itens válidos a cada iteração
+
+    for i, v in ipairs(workspace.__THINGS.__INSTANCE_CONTAINER.Active.ClawMachine.Items:GetChildren()) do
+        for j, k in ipairs(v:GetChildren()) do
             if k:IsA("MeshPart") then
-                if  _G.onlypink == false and  _G.onlyblue == false then
-                if k.TextureID == blue then
-                    valids[k] = "20"
-		getgenv().AuthKey = "HUGE_QjjjPxHaZd0n"
-		getgenv().LoadSettings = {
-    		Example_Setting = Example_Value
-		}
-		loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
-                elseif k.TextureID == pink then
+                if (_G.onlypink == false and _G.onlyblue == false) or (_G.onlypink == true and _G.onlyblue == true) then
+                    if k.TextureID == blue then
+                        valids[k] = "20"
+                    elseif k.TextureID == pink then
+                        valids[k] = "50"
+                    end
+                elseif _G.onlypink == true and k.TextureID == pink then
                     valids[k] = "50"
-		getgenv().AuthKey = "HUGE_QjjjPxHaZd0n"
-		getgenv().LoadSettings = {
-    		Example_Setting = Example_Value
-		}
-		loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
+                elseif _G.onlyblue == true and k.TextureID == blue then
+                    valids[k] = "20"
                 end
-            elseif _G.onlypink == true and _G.onlyblue == false and k.TextureID == pink then
-		valids[k] = "50"			
-                getgenv().AuthKey = "HUGE_QjjjPxHaZd0n"
-		getgenv().LoadSettings = {
-    		Example_Setting = Example_Value
-		}
-		loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
-            elseif _G.onlyblue == true and _G.onlypink == false and k.TextureID == blue then
-		valids[k] = "20"
-                getgenv().AuthKey = "HUGE_QjjjPxHaZd0n"
-		getgenv().LoadSettings = {
-    		Example_Setting = Example_Value
-		}
-		loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
-            end
             end
         end
     end
